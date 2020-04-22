7d1219dba66a8e65aad51ed84cae5d1540eff4b8
C18356933 <55754262+C18356933@users.noreply.github.com>
2020-04-22 15:35:09 +0100

    rectangles on graph with color

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -5,19 +5,19 @@ import processing.core.PApplet;
 import processing.data.Table;
 import processing.data.TableRow;
 
-public class Gantt extends PApplet
-{	
+public class Gantt extends PApplet{	
 	public ArrayList<Task> tasks = new ArrayList<Task>();
 	private float Left;
 	private float Margin;
+	private int clickLeft = -1;
+	private int clickRight = -1;
+	int Number=30;
 
-	public void settings()
-	{
+	public void settings(){
 		size(800, 600);
 	}
 
-	public void loadTasks()
-	{
+	public void loadTasks(){
 		Table table = loadTable("tasks.csv", "header");
 		for (TableRow row : table.rows()) {
 			Task ta = new Task(row);
@@ -25,55 +25,69 @@ public class Gantt extends PApplet
 		}
 	}
 
-	public void printTasks()
-	{
-		for(Task t : tasks)
-		{
+	public void printTasks(){
+		for(Task t : tasks){
 			println(t);
 		}
 	} 
 
 	public void displayTasks(){
-		int Number=30;
 		float X;
+		float Y;
 		float textHeight=0.8f;
+		float rectHeight=50;
+		float rectStart;
+		float rectEnd; 
+		float rectWidth;
+		float color;
+		float r=8;
 
 		stroke(255);
 		fill(255);
 		textAlign(LEFT);
 
-		for(int i=1;i<=Number;i++)
-		{
+		for(int i=1;i<=Number;i++){
 			X=map(i,1,Number,Left,width-Margin);
 			line(X,Margin,X,height-Margin);
 
 			text(i,X,Margin*textHeight);
+		}
+
+		for(int i=0;i<tasks.size();i++){
+			fill(255);
+			Y=map(i,0,tasks.size(),2*Margin,height-Margin);
+			text(tasks.get(i).getName(),Margin,Y);
+
+			noStroke();
+			color=map(i,0,tasks.size(),0,255);
+			fill(color,255,255);
+			
+			rectStart=map(tasks.get(i).getStart(),1,Number,Left,width-Margin);
+			rectEnd=map(tasks.get(i).getEnd(),1,Number,Left,width-Margin);
+			rectWidth=rectEnd-rectStart;
+
+			rect(rectStart,Y-rectHeight/2,rectWidth,rectHeight,r);
 		}
 		
 	}
 	
 	public void mousePressed()
 	{
-		println("Mouse pressed");	
+		
 	}
 
-	public void mouseDragged()
-	{
-		println("Mouse dragged");
+	public void mouseDragged(){
+		
 	}
-
 	
-	
-	public void setup() 
-	{
+	public void setup() {
 		Left = width / 6;
 		Margin = width / 20;
 		loadTasks();
 		printTasks();
 	}
 	
-	public void draw()
-	{			
+	public void draw(){			
 		colorMode(HSB);
 		background(0);
 		displayTasks();
