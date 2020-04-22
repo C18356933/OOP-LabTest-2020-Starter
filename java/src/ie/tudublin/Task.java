82af0a4038fd992aaf1a2df27f2a422435a2912e
C18356933 <55754262+C18356933@users.noreply.github.com>
2020-04-22 13:36:02 +0100

    added fields,methods and constructors to Task class

--- java/src/ie/tudublin/Task.java
+++ java/src/ie/tudublin/Task.java
@@ -1,5 +1,43 @@
 package ie.tudublin;
 
-public class Task {
+import processing.data.TableRow;
 
+public class Task {
+    private String Name;
+    private int Start;
+    private int End;
+
+    public Task(String Name, int Start, int End){
+        this.Name = Name;
+        this.Start = Start;
+        this.End = End;
+    }
+
+    public String getName(){
+        return Name;
+    } 
+    
+    public int getStart(){
+        return Start;
+    }
+    
+    public int getEnd(){
+        return End;
+    }
+
+    public void setName(String Name){
+        this.Name = Name;
+    }
+
+    public void setStart(int Start){
+        this.Start = Start;
+    }
+
+    public void setEnd(int End){
+        this.End = End;
+    }
+
+    public Task(TableRow tr){
+        this(tr.getString("Task"),tr.getInt("Start"),tr.getInt("End"));
+    }
 }
